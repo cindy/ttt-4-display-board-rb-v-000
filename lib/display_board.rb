286b2097ac29c5a47515913b1f91a47fdfde5970
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-19 22:10:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  board = board.each_slice(3).to_a
   filled_board = []
+  board = board.each_slice(3).to_a
   board.each do |a|
     filled_board << a.join(" | ")
   end
